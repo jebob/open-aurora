25a1bd26611d490c6d244ab3469c2912fc601013
jebob <robert@howletts.org.uk>
2017-03-12 18:25:10 +0000

    Fix broken merge

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import math
+import sys
+import random
+from PyQt5.QtWidgets import QMainWindow, QFrame, QDesktopWidget, QApplication, QPushButton, QAction, qApp, \
+    QGridLayout, QVBoxLayout, QHBoxLayout, QWidget
+from PyQt5.QtGui import QPainter, QColor, QIcon
+
 eventQueue = []  # todo this properly later
 
 
@@ -54,36 +60,6 @@ d = sqrt(x*x+y*y)
 rate of change = 0.5*(2x*dx/dt + 2y*dy/dt)/sqrt(x*x+y*y)
 
 """
-=======
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
-
-"""
-ZetCode PyQt5 tutorial 
-
-This is a Tetris game clone.
-
-author: Jan Bodnar
-website: zetcode.com 
-last edited: January 2015
-"""
-
-import sys
-import random
-from PyQt5.QtWidgets import QMainWindow, QFrame, QDesktopWidget, QApplication, QPushButton, QAction, qApp, \
-    QGridLayout, QVBoxLayout, QHBoxLayout, QWidget
-from PyQt5.QtGui import QPainter, QColor, QIcon
-
-
-class GamePiece:
-    detectRange = 0  # by default things don't detect
-    speed = 0  # by default things don't move
-    """"This is a thing class. It represents an object or coll"""
-    def __init__(self, position):
-        assert isinstance(position, tuple)
-        assert len(position) == 2
-        assert all(isinstance(n, int) for n in position)
-        self.position = position
 
 
 class GameState:
@@ -93,8 +69,8 @@ class GameState:
         self.set_demo()
 
     def set_demo(self):
-        demo_posns = [(1, 2), (4, 5), (3, -1)]
-        self.pieceList = [GamePiece(posn) for posn in demo_posns]
+        demo_posns = [1+2j, 4+5j, 3-1j]
+        self.pieceList = [GamePiece(posn, 0.0) for posn in demo_posns]
 
 
 class OpenAurora(QMainWindow):
@@ -154,8 +130,7 @@ class TacView(QFrame):
     def __init__(self, state):
         super().__init__()
 
-        self.curX = 0
-        self.curY = 0
+        self.curPosn = 0+0j
         self.curScale = 100.0
 
         self.state = state
@@ -168,14 +143,14 @@ class TacView(QFrame):
         painter.fillRect(0, 0, rect.right(), rect.bottom(), QColor(0x000000))
 
         for piece in self.state.pieceList:
-            self.draw_ship(painter, piece.position[0] * self.curScale, piece.position[1] * self.curScale)
+            self.draw_ship(painter, (piece.start_position + self.curPosn) * self.curScale)
             pass
 
     @staticmethod
-    def draw_ship(painter, x, y):
+    def draw_ship(painter, posn):
         color = QColor(0x6666CC)
         width = 10
-        painter.fillRect(x, y, width, width, color)
+        painter.fillRect(posn.real, posn.imag, width, width, color)
 
 if __name__ == '__main__':
     app = QApplication([])
